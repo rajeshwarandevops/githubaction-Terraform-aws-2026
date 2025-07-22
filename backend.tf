813f15dd666116195baaded3b0770e07cf9ecd8c
Rajeshwaran <kbrajeshwarangit@gmail.com>
2025-07-23 02:17:19 +0300

    s3 backend added

---
+++ backend.tf
@@ -0,0 +1,13 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-github-action-23072025"
+    key            = "devopsforu/terraform.tfstate"
+    region         = "ap-southeast-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks" # optional for state locking
+  }
+
+
+
+
